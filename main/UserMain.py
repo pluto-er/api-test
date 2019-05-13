2dd37cdf238de41e74e2fd25277924c1d9f9c57f
wangming <1357282015@qq.com>
2019-05-13 15:25:13 +0800

    fix

--- main/UserMain.py
+++ main/UserMain.py
@@ -38,21 +38,21 @@ class UserMain:
 		self.startup.start_up()  # 启动页面
 		self.shop.start_up()  # 店铺相关
 		self.goods.my_data()  # 菜品列表
-		self.goods.my_order()  # 点餐-确认页
-		self.goods.pay_success()  # 支付完成
-		self.goods.poster()  # 广告位
-		self.msg.start_up()  # 消息
-		self.user.my_user()
-		self.user.my_coupon()
-		self.user.my_recharge()
-		self.user.my_invitations()
-		self.user.my_order()
-		self.order.add_order(1)  # 库存情况方式
-		self.order.add_order(2)  # 库存情况方式
-		self.order.add_order(3)  # 库存情况方式
-		self.order.add_order(4)  # 库存情况方式
-		self.order.add_order(5)  # 库存情况方式
-		self.user.my_comment()
+		# self.goods.my_order()  # 点餐-确认页
+		# self.goods.pay_success()  # 支付完成
+		# self.goods.poster()  # 广告位
+		# self.msg.start_up()  # 消息
+		# self.user.my_user()
+		# self.user.my_coupon()
+		# self.user.my_recharge()
+		# self.user.my_invitations()
+		# self.user.my_order()
+		# self.order.add_order(1)  # 库存情况方式
+		# self.order.add_order(2)  # 库存情况方式
+		# self.order.add_order(3)  # 库存情况方式
+		# self.order.add_order(4)  # 库存情况方式
+		# self.order.add_order(5)  # 库存情况方式
+		# self.user.my_comment()
 
 		# 写入html文件
 		self.get_html_data.set_html()
